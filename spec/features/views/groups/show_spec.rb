3061de0af03e11f8c4714bb34f419d42b45f9e03
Kevin Ta <kvnta@yahoo.com>
2023-01-06 14:12:12 -0700

    Add about to view page. Fix test for about show page

--- spec/features/views/groups/show_spec.rb
+++ spec/features/views/groups/show_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Groups' do
    describe '#show' do 
+    it 'page has name of group and about on page' do 
+      chipmunk_smackdown = Group.create!(name: "2022 Chipmunk Smackdown", about: "It's going to be nutz")
 
-    chipmunk_smackdown = Group.create!(name: "2022 Chipmunk Smackdown", about: "It's going to be nutz")
+      visit group_path(chipmunk_smackdown)
 
-    visit group_path(chipmunk_smackdown)
-
-    expect(page).to have_content(chipmunk_smackdown.name)
-    expect(page).to have_content(chipmunk_smackdown.about)
+      expect(page).to have_content(chipmunk_smackdown.name)
+      expect(page).to have_content(chipmunk_smackdown.about)
    end
-
-
+  end 
 end
