d90c8a53862681595fa8f033900dad4a99538c31
Kevin Ta <kvnta@yahoo.com>
2023-01-06 14:08:30 -0700

    test for show page. name appears and about appears on page.

---
+++ spec/features/views/groups/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Groups' do
+   describe '#show' do 
+
+    chipmunk_smackdown = Group.create!(name: "2022 Chipmunk Smackdown", about: "It's going to be nutz")
+
+    visit group_path(chipmunk_smackdown)
+
+    expect(page).to have_content(chipmunk_smackdown.name)
+    expect(page).to have_content(chipmunk_smackdown.about)
+   end
+
+
+end
