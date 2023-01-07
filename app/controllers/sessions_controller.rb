6ccd2df9e7568e688344cfc6c20351519de65a3d
Sage Skaff <skaffsage@gmail.com>
2023-01-06 17:42:37 -0800

    Update user params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
     def create
-        user = User.find_or_create_by(email: user_params[email])
+        user = User.find_or_create_by(email: user_params[:email])
         user.update(user_params)
         session[:user_id] = user.id
         redirect_to root_path
     end
 
-    def destroy
-        session[:user_id] = nil
-        redirect_to root_path
-    end
+    # def destroy
+    #     session[:user_id] = nil
+    #     redirect_to root_path
+    # end
 
     private
 
@@ -20,8 +20,8 @@ class SessionsController < ApplicationController
     def user_params
         # will need to add token for auth and uid
         {
-        email = auth_hash['info']['email'],
-        name = auth_hash['info']['name']
+        email: auth_hash['info']['email'],
+        name: auth_hash['info']['name']
         }
     end
 end
