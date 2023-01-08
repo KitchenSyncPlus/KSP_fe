b05cb401bca3339d3955160a2416a73ddc3087f1
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-01-08 16:39:33 -0700

    Adds feature to add user

--- spec/features/views/users/index_spec.rb
+++ spec/features/views/users/index_spec.rb
@@ -1,18 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Users' do
+  before(:each) do
+    10.times do
+      create(:user)
+    end
+
+    @users = User.all
+
+    visit users_path
+  end
+
   describe '#index' do
     it 'shows all current users as links' do
-      10.times do
-        create(:user)
-      end
-
-      users = User.all
-
-      visit users_path
-
-      users.each do |user|
+      @users.each do |user|
         expect(page).to have_link(user.name)
+      end
+    end
+
+    context "add a user" do
+      it 'has the ability to add a user' do
+        expect(page).to have_link('add user')
+        click_on 'add user'
+        expect(current_path).to eq(new_user_path)
       end
     end
   end
