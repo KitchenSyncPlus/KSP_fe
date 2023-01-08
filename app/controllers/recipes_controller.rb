fc9da600c4d51d93b69d0f2d2c0a8af0afe4d40d
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-01-07 18:06:43 -0700

    Searches and returns list of ingredients
    - [x] I have performed a self-review of my code
    - [x] I have added thorough tests.
    - [x] I have commented my code, if needed
    
    good
    
    Co-authored-by: Kevin Ta <36166420+KevinT001@users.noreply.github.com>
    Co-authored-by: Sage Skaff <99838997+sage-skaff@users.noreply.github.com>

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,12 @@
 class RecipesController < ApplicationController
   def index
+    if params.has_key? :search
+      # search for the recipe
+      @search_term = params[:search]
+      @ingredients = FoodFacade.new.recipes(params[:search])
+    else
+      # don't search for the recipe
+    end
   end
 
   def show
