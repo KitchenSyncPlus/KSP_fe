9a7668508633a6ba736438a9b73dfee682c5983f
Sage Skaff <skaffsage@gmail.com>
2023-01-07 16:12:22 -0800

    Add test for group user count

--- spec/features/views/groups/show_spec.rb
+++ spec/features/views/groups/show_spec.rb
@@ -24,5 +24,16 @@ RSpec.describe 'Groups' do
       expect(page).to have_content(theo.name)
       expect(page).to_not have_content(simon.name)
    end
+
+   it 'page has count of total users in the group' do 
+      chipmunk_smackdown = Group.create!(name: "2022 Chipmunk Smackdown", about: "It's going to be nutz")
+      fish_fighters = Group.create!(name: "2022 Fish Fighters", about: "Step on the Scale")
+      alvin = chipmunk_smackdown.users.create!(name: "Alvin", email: "Acorn@tree.com")
+      theo = chipmunk_smackdown.users.create!(name: "Theodore", email: "Walnut@tree.com")
+      simon = fish_fighters.users.create!(name: "Simon", email: "outcastvermin@trap.com")
+      visit group_path(chipmunk_smackdown)
+
+      expect(page).to have_content('Group Members: 3')
+   end
   end 
 end
